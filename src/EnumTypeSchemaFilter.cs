e862c3b3bf1ec518cd473e3b617a00d8f0ffed6a
Nov Choun <nov.choun@gmail.com>
2017-12-01 11:12:24 +0800

    add extensions

---
+++ src/EnumTypeSchemaFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Swashbuckle.SchemaExtensions.Core
+{
+    public class EnumTypeSchemaFilter  : ISchemaFilter
+    {
+        public void Apply(Schema model, SchemaFilterContext context)
+        {
+            if (context.SystemType.GetTypeInfo().IsEnum)
+            {
+                // Add enum type information
+                model.Extensions.Add("x-ms-enum", new
+                {
+                    name = context.SystemType.Name,
+                    modelAsString = false
+                });
+            }
+        }
+    }
+}
---
+++ src/SwaggerConfigExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.SchemaExtensions.Core;
+
+namespace Swashbuckle.SchemaExtensions.Core
+{
+    public static class SwaggerConfigExtensions
+    {
+        public static void AddEnumTypeFilter(this SwaggerGenOptions options)
+        {
+            options.SchemaFilter<EnumTypeSchemaFilter>();
+        }
+    }
+}
